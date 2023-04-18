3cb34feac0d0fd411ca0d7321a49171937c7ed88
DavideFarci <yank92.df@gmail.com>
2023-04-18 17:39:19 +0200

    add snack 2.3

--- snack 2.3/script.js
+++ snack 2.3/script.js
@@ -3,18 +3,16 @@ Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in 
 */
 
 // creo un array di numeri interi 
-const eleWhole = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-const eleOdd = 0;
+let eleWhole = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+let eleOdd = 0;
 
 // creo un ciclo che scorra tutto eleWhole ogni due valori 
-for (i = 0; i < eleWhole.length; i+2) {
+for (i = 0; i < eleWhole.length; i++) {
     if (i & 2 !== 0) {
         eleOdd += eleWhole[i];
     }
 }
 
+console.log("La somma dei numeri in posizione dispari è:");
 console.log(eleOdd);
 
-
-
-// const eleOdd = (eleOdd % )
